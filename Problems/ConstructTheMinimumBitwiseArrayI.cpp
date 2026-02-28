e52d33da4a78ebf89139876eeea065749f08e840
NorhanIhab <68747567+MiraculouN@users.noreply.github.com>
2026-02-28 22:39:16 +0200

    Add solution: ConstructTheMinimumBitwiseArrayI

---
+++ Problems/ConstructTheMinimumBitwiseArrayI.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1934077509 lang=cpp
+ *
+ * ConstructTheMinimumBitwiseArrayI
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int>v(2005, 1005);
+        v[2] = -1;
+        for(int i=2;i<1005;i++){
+            int x = i | (i-1);
+            v[x] = min(v[x], i-1);
+        }
+        vector<int>ans;
+        for(auto it:nums){
+            ans.push_back(v[it]);
+        }
+        return ans;
+    }
+};
