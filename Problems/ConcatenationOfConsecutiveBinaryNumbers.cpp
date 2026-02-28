3b319d812b8df8a2589007f7964f9398db58553f
NorhanIhab <68747567+MiraculouN@users.noreply.github.com>
2026-02-28 16:18:27 +0200

    Add solution: ConcatenationOfConsecutiveBinaryNumbers

---
+++ Problems/ConcatenationOfConsecutiveBinaryNumbers.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1933222824 lang=cpp
+ *
+ * ConcatenationOfConsecutiveBinaryNumbers
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    const int N=1e9+7;
+    int concatenatedBinary(int n) {
+        long long int ans =0;
+        for(int i=1;i<=n;i++){
+            long long int x = i, cnt=1;
+            while(x){
+                cnt=(cnt * 2)%N;
+                x/=2;
+            }
+            ans = (ans * cnt)%N;
+            ans = (ans + i)%N;
+        }
+        return ans;
+    }
+};
