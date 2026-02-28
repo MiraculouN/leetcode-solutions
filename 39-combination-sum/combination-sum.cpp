105ba989fb3015e58a0c45e19894b9fba4af30b5
NorhanIhab <68747567+MiraculouN@users.noreply.github.com>
2026-02-28 15:54:45 +0200

    Time: 0 ms (100.00%) | Memory: 14.4 MB (33.02%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int sum =0, t, n;
+    int t, n;
     vector<int>cur;
     vector<vector<int>>ret;
     vector<int>v;
 
-    void fun(int idx){
+    void fun(int idx, int sum){
         if(sum > t)return ;
         if(idx==n){
             if(sum == t)ret.push_back(cur);
             return ;
         }
         /// take v[idx] (do)
-        sum += v[idx];
+        ///sum += v[idx];
         cur.push_back(v[idx]);
-        fun(idx);
+        fun(idx, sum + v[idx]);
         ///leave v[idx]; (undo)
 
-        sum -= v[idx];
+        ////sum -= v[idx];
         cur.pop_back();
-        fun(idx+1);
+        fun(idx+1, sum);
 
     }
     vector<vector<int>> combinationSum(vector<int>& c, int target) {
         t = target;
         v=c;
         n = c.size();
-        fun(0);
+        fun(0,0);
         return ret;
     }
 };
