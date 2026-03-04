1e037b3bc9da19238efc9d731c3ddf3880ac2b31
NorhanIhab <68747567+MiraculouN@users.noreply.github.com>
2026-03-04 02:08:32 +0200

    Add solution: CustomerWhoVisitedButDidNotMakeAnyTransactions

--- Problems/CustomerWhoVisitedButDidNotMakeAnyTransactions.py
+++ Problems/CustomerWhoVisitedButDidNotMakeAnyTransactions.py
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1937156171 lang=mssql
+ * @lc app=leetcode id=1937203256 lang=mssql
  *
  * CustomerWhoVisitedButDidNotMakeAnyTransactions
  * 
- * Difficulty: Level
+ * Difficulty: Easy
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -11,8 +11,6 @@
 
 /* Write your T-SQL query statement below */
 
-SELECT  customer_id , COUNT(V.visit_id) as count_no_trans 
-from Visits V left join Transactions T
-on V.visit_id = T.visit_id
-where T.transaction_id is NULL
-group by customer_id
+SELECT customer_id, COUNT(visit_id) as count_no_trans from Visits
+where visit_id not in (SELECT visit_id from Transactions )
+group by customer_id;
