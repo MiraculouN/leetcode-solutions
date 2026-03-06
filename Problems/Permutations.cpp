d84c76437c59d061e8ccf32987b9a925ffac9d77
NorhanIhab <68747567+MiraculouN@users.noreply.github.com>
2026-03-06 21:25:17 +0200

    Add solution: Permutations

--- Problems/Permutations.cpp
+++ Problems/Permutations.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1939819896 lang=cpp
+ * @lc app=leetcode id=1939823909 lang=cpp
  *
  * Permutations
  * 
- * Difficulty: Medium
+ * Difficulty: Level
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -11,32 +11,16 @@
 
 class Solution {
 public:
-    int n;
-    vector<int>cur, used;
-    vector<vector<int>>ret;
-    vector<int>v;
-    void generate_permutation(){
-        if(cur.size()==n){
-            ret.push_back(cur);
-            return ;
-        }
-        for(int i=0;i<n;i++){
-            if(!used[i]){
-                cur.push_back(v[i]);
-                used[i]=1;
-                generate_permutation();
-
-                cur.pop_back();
-                used[i]=0;
-            }
-        }
-    }
+    
     vector<vector<int>> permute(vector<int>& nums) {
-        n=nums.size();
-        used.assign(n,0);
-        v=nums;
-        generate_permutation();
+        int n=nums.size();
+        vector<vector<int>>ret;
+        sort(nums.begin(), nums.end());
+
+        do{
+            ret.push_back(nums);
+        }while(next_permutation(nums.begin(), nums.end()));
         return ret;
-        //// used[i]=0 element nums[i] can be put in cur
+        
     }
 };
