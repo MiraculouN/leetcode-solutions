b3a848641835cdefef331cfe3321f64e94f8dab0
NorhanIhab <68747567+MiraculouN@users.noreply.github.com>
2026-03-12 17:01:15 +0200

    Add solution: StudentsAndExaminations

--- Problems/StudentsAndExaminations.py
+++ Problems/StudentsAndExaminations.py
@@ -1,5 +1,5 @@
 /*
- * @lc app=leetcode id=1944517256 lang=mssql
+ * @lc app=leetcode id=1944519416 lang=mssql
  *
  * StudentsAndExaminations
  * 
@@ -25,54 +25,3 @@ order by S.student_id , U.subject_name
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*SELECT S.student_id, S.student_name, SS.subject_name ,COUNT(E.subject_name)
-from Students S cross join Subjects SS cross join Examinations E
-where S.student_id = E.student_id and E.subject_name = SS.subject_name
-group by S.student_id
-order by S.student_id, subject_name
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT S.student_id, S.student_name , E.subject_name, COUNT(subject_name)as attended_exams
-from Students S cross join Examinations E 
-where E.student_id = S.student_id
-GROUP BY
-    S.student_id,
-    S.student_name,
-    E.subject_name
