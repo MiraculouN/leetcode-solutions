15c18f82ef73cb002e637e91b07fe99979096b90
NorhanIhab <68747567+MiraculouN@users.noreply.github.com>
2026-04-28 02:05:28 +0300

    Add solution: FriendRequestsIiWhoHasTheMostFriends

--- Problems/FriendRequestsIiWhoHasTheMostFriends.py
+++ Problems/FriendRequestsIiWhoHasTheMostFriends.py
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1989544707 lang=mssql
+ * @lc app=leetcode id=1989551494 lang=mssql
  *
  * FriendRequestsIiWhoHasTheMostFriends
  * 
- * Difficulty: Level
+ * Difficulty: Medium
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -11,26 +11,13 @@
 
 /* Write your T-SQL query statement below */
 
-With Requester As(
-    SELECT R1.requester_id as id , COUNT(R1.accepter_id) as num
-    FROM RequestAccepted R1 
-    GROUP BY R1.requester_id
-), 
-Accepter AS(
-    SELECT accepter_id as id, COUNT(requester_id) as num
-    FROM RequestAccepted
-    GROUP BY accepter_id 
-),
-Rank AS(
-    SELECT COALESCE(R.id, A.id) as id , COALESCE(R.num,0) + COALESCE(A.num,0) as num
-    FROM Requester R FULL OUTER JOIN Accepter A
-    ON R.id = A.id
-    
+WITH Union_table AS(
+    SELECT requester_id as id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id FROM RequestAccepted
 )
---SELECT * FROM Requester
---SELECT * FROM Accepter
 
-SELECT  top(1) id,  num FROM Rank
-ORDER BY num desc
-
-
+SELECT  top(1) id, COUNT(id) as num
+FROM Union_table
+GROUP BY id
+ORDER BY COUNT(id) desc
